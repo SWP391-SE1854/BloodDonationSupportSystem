a87a536c08e3d9f0dcea1f0b249f953a21fbe278
GiaHuy83 <tugiahuy05@gmail.com>
2025-06-15 13:36:20 +0700

    update ci

--- BusinessLogic/IServices/IJwtService.cs
+++
@@ -1,9 +0,0 @@
-﻿using BloodDonationSystem.DataAccess.Entities;
-
-namespace BloodDonationSystem.BusinessLogic.IServices
-{
-    public interface IJwtService
-    {
-        string GenerateToken(User user);
-    }
-}
